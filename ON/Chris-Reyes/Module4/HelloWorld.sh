e4d50a91fc3115b2f12db51d22a2ff572a7136c1
EC2 Default User <ec2-user@ip-172-31-23-66.us-west-1.compute.internal>
2020-08-18 23:30:46 +0000

    Submission for Module 5

--- ON/Chris-Reyes/Module4/HelloWorld.sh
+++ ON/Chris-Reyes/Module4/HelloWorld.sh
@@ -1,4 +1,12 @@
 #! /bin/bash
 printf "Hello %s!\n" "$1"
 printf "Value of all the positional parameters: %s\n" "$*"
+printf "The number of positional parameters: %s\n" "$*"
+printf "The name of the current running script: %s\n" "$0"
+printf "The process identification nuber (PID): %s\n" "$$"
+printf "The exit code of the last-executed command: %s\n" "$?"
+
+#Define a variable and use it
+newName="Arthur Wang"
+printf "I have a cool name which is %s\n" "$newName"
 
