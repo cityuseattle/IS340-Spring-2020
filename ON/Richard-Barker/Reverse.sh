69062715e3cc7b01a8173e85c208dd6fe46eb3f9
RichAzure <barkerrichard@cityuniversity.edu>
2020-09-18 04:23:47 +0000

    Submission for Module 9

---
+++ ON/Richard-Barker/Reverse.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+reverse()	{
+	str=$1
+	seversedStr=
+	while  [ -n "$str"  ]
+	do
+		temp=${str%?}
+		reversedStr=$reversedStr${str#$temp}
+		str=$temp
+	done
+	printf "The reversed string is: %s\n" "$reversedStr"
+}
